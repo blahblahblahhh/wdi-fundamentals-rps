875232b150c7e9ff4a8512d66fdaa65405e5d1ec
Sophie <sophieadamswastaken@gmail.com>
2015-07-14 14:45:43 -0400

    ADDED PLAYERMOVE/COMPUTERMOVE

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -41,7 +41,41 @@ function getWinner(playerMove,computerMove) {
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     /* YOUR CODE HERE */
+    if (playerMove === "rock") {
+        if (computerMove === "rock") {
+            winner = "tie";
+        }
+        else if (computerMove === "paper") {
+            winner = "computer";
+        }
+        else {
+            winner = "player";
+        }
+    }
+    else if (playerMove === "paper") {
+        if (computerMove === "paper") {
+            winner = "tie";
+        }
+        else if (computerMove === "scissors") {
+            winner = "computer";
+        }
+        else {
+            winner = "player";
+        }
+    }
     return winner;
+    
+    else (playerMove === "scissors") {
+        if (computerMove === "scissors") {
+            winner = "tie";
+        }
+        else if (computerMove === "rock") {
+            winner = "computer";
+        }
+        else {
+            winner = "player"
+        }
+
 }
 
 function playToFive() {
