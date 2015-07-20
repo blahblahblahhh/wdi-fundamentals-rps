7f4d108deb85213adc348b59f928cd47d909609c
Sophie <sophieadamswastaken@gmail.com>
2015-07-19 21:52:46 -0400

    GAME PLAYS HAPPILY

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -52,19 +52,15 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
-    var winner = getWinner(playerMove,computerMove);
-    while (playerWins < 5 && computerWins < 5) {
-        if (winner === "Player") {
-            playerWins += 1;
-        }
-        else if (winner === "Computer") {
-            computerWin +=1;
-        }
-        getInput()
-        randomPlay();
+        while (playerWins < 5 && computerWins < 5) {
+        var playerMove = getPlayerMove();
+        var computerMove = getComputerMove();
+        var winner = getWinner(playerMove, computerMove);
+        if (winner === 'player') {playerWins++}
+        else {computerWins++}
+        console.log("Player chose " + playerMove + " while Computer chose " + computerMove);
+        console.log("The score is " + playerWins + " to " + computerWins + "\n");
+    }
     return [playerWins, computerWins];
 }
-
 playToFive();
